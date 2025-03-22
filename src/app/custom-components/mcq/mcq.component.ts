91962ed7dcf6a78f32e72078cb67e9e78bfdc09c
aniket <kardileaniket243@gmail.com>
2025-03-23 00:50:08 +0530

    add

--- src/app/custom-components/mcq/mcq.component.ts
+++ src/app/custom-components/mcq/mcq.component.ts
@@ -1,23 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mcq',
   standalone: false,
   templateUrl: './mcq.component.html',
-  styleUrls: ['./mcq.component.css']
+  styleUrl: './mcq.component.css'
 })
 export class McqComponent {
-  @Input() currentQuestionIndex: number = 0;
-  @Input() questions: any[] = [];
-  @Output() answerSelected = new EventEmitter<string>();
-  selectedAnswer: string | null = null;
 
-  get currentQuestion() {
-    return this.questions[this.currentQuestionIndex];
-  }
-
-  onSelect(answer: string) {
-    this.selectedAnswer = answer;
-    this.answerSelected.emit(answer);
-  }
 }
